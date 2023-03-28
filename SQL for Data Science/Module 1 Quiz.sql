e9ae5ec07d822b284edd48c68fc4254e285a1b05
vfmota <victor-mota98@hotmail.com>
2023-03-28 17:15:42 -0600

    Q

---
+++ SQL for Data Science/Module 1 Quiz.sql
@@ -0,0 +1,13 @@
+--P1 Retrieve all the records from the Employees table.
+
+Select * from Employees
+
+----------------------------------------------------------------
+
+--P2 Retrieve the FirstName, LastName, Birthdate, Address, City, and State from the Employees table.
+
+select FirstName, LastName, Birthdate, Address, City, State from Employees
+
+-----------------------------------------------------------------
+
+--P3 
